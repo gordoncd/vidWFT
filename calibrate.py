1016d95f5293c483b91446a521c755b49fc9461a
Gordon Doore <gcdoor25@colby.edu>
2024-07-11 12:00:36 -0400

    calibration now in good form

--- calibrate.py
+++ calibrate.py
@@ -9,74 +9,138 @@ import numpy as np
 import cv2
 import glob
 import yaml #type: ignore
+import os
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+def calibrate_camera(src : str, dest : str, base_filename : str = '', chessboard_size : tuple = (6,9), show = False, verbose = False):
+    '''
+    calibrate camera using chessboard images
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((63,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:9].T.reshape(-1,2)
+    Args: 
+        src (str): path to folder containing calibration images
+        dest (str): path to folder to save camera matrices
+        base_filename (str): prefix for saved files
+        chessboard_size (tuple): size of chessboard used for calibration
+        show (bool): whether to show undistorted images
+    '''
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# store points
-objpoints = [] # made up for the most part
-imgpoints = [] # points
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    num_sq = chessboard_size[0]*chessboard_size[1]
+    objp = np.zeros((num_sq,3), np.float32)
+    objp[:,:2] = np.mgrid[0:chessboard_size[0],0:chessboard_size[1]].T.reshape(-1,2)
 
-# list of image filepaths
-images = glob.glob('/Users/gordondoore/Documents/GitHub/waves-summer-2024/calibration/acortiz@colbydotedu_CALIB/calib_frames_5k/*.jpg') 
+    # store points
+    objpoints = [] # made up for the most part
+    imgpoints = [] # points
 
-for fname in images:
-    img = cv2.imread(fname)
-    #img = cv2.resize(img, (350,350))
+    # list of image filepaths
+    images = glob.glob(src+'/*.jpg') 
 
-    gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # find corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,9), flags = cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
-    #add points to image
-    if ret:
-        objpoints.append(objp)
+    for fname in images:
+        img = cv2.imread(fname)
+        #img = cv2.resize(img, (350,350))
 
-        # draw corners
-        img = cv2.drawChessboardCorners(img, (7,9), corners, ret)
-        imgpoints.append(corners)
-        cv2.imshow('img', img)
+        gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # find corners
+        ret, corners = cv2.findChessboardCorners(gray, (chessboard_size[0],chessboard_size[1]), flags = cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+        #add points to image
+        if ret:
+            objpoints.append(objp)
+
+            # draw corners
+            img = cv2.drawChessboardCorners(img, (chessboard_size[0],chessboard_size[1]), corners, ret)
+            imgpoints.append(corners)
+            if show:
+                cv2.imshow('img', img)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+        else: 
+            print('Could not find corners in image: ',fname)
+
+    # calibrate camera
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None) #type: ignore
+
+    if verbose: 
+        print("Camera matrix : \n")
+        print(mtx)
+        print("dist : \n")
+        print(dist)
+        print("rvecs : \n")
+        print(rvecs)
+        print("tvecs : \n")
+        print(tvecs)
+
+    if show:
+        # Undistort image
+        undistorted_img = cv2.undistort(img, mtx, dist, None, mtx)
+
+        # Display undistorted image
+        cv2.imshow('Undistorted Image', undistorted_img)
         cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-cv2.destroyAllWindows()
+    #save camera matrices to yaml file: 
+    data = {
+        'camera_matrix': mtx.tolist(),
+        'dist_coeff': dist.tolist(),
+        'rvecs': [rvec.tolist() for rvec in rvecs],
+        'tvecs': [tvec.tolist() for tvec in tvecs]
+    }
+    #make dir if it does not exist
+    if not os.path.exists(dest+'/' + base_filename+'vid_camera_matrices.yaml'):
+        if verbose: 
+            print('could not find file: ',dest+'/' + base_filename+'vid_camera_matrices.yaml'+'\ncreating directory')
+        os.makedirs(dest)
+    with open(dest+'/' + base_filename+'vid_camera_matrices.yaml', 'w') as file:
+        yaml.dump(data, file)
 
-# calibrate camera
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None) #type: ignore
+    #also save as numpy arrays: 
 
-print("Camera matrix : \n")
-print(mtx)
-print("dist : \n")
-print(dist)
-print("rvecs : \n")
-print(rvecs)
-print("tvecs : \n")
-print(tvecs)
+    np.save(dest+'/' + base_filename + 'camera_matrix.npy', mtx)
+    np.save(dest+'/' + base_filename + 'dist_coeff.npy', dist)
+    np.save(dest+'/' + base_filename + 'rvecs.npy', rvecs)
+    np.save(dest+'/' + base_filename + 'tvecs.npy', tvecs)
 
-# Undistort image
-undistorted_img = cv2.undistort(img, mtx, dist, None, mtx)
+def load_camera_calibration_data(matrix_path : str, distance_coefficient_path : str) -> tuple:
+    '''
+    load camera calibration data from saved files
 
-# Display undistorted image
-cv2.imshow('Undistorted Image', undistorted_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    Args: 
+        matrix_path (str): path to camera matrix file
+        distance_coefficient_path (str): path to distance coefficient file
 
-#save camera matrices to yaml file: 
-data = {
-    'camera_matrix': mtx.tolist(),
-    'dist_coeff': dist.tolist(),
-    'rvecs': [rvec.tolist() for rvec in rvecs],
-    'tvecs': [tvec.tolist() for tvec in tvecs]
-}
+    Returns:
+        tuple: camera matrix and distance coefficients
+    '''
+    mtx = np.load(matrix_path)
+    dist = np.load(distance_coefficient_path)
+    return mtx, dist
 
-with open('/Users/gordondoore/Documents/GitHub/waves-summer-2024/gopro_5k_vid_camera_matrices.yaml', 'w') as file:
-    yaml.dump(data, file)
+def extract_calibration_frames(filepath : str, nframes : int) ->list[np.ndarray]:
+    '''
+    randomly grab nframes frames from the mp4 at
+    filepath and return them as a list of numpy arrays 
+    
+    Args: 
+        filepath (str): path to video file
+        nframes (int): number of frames to extract
+    '''
+    #randomly grab nframes frames from the mp4 at
+    # filepath and return them as a list of numpy arrays 
+    cap = cv2.VideoCapture(filepath)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_indices = np.random.choice(total_frames, nframes, replace=False)
+    frames = []
+    for index in frame_indices:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, index)
+        ret, frame = cap.read()
+        if ret:
+            frames.append(frame)
+    cap.release()
+    return frames
 
-#also save as numpy arrays: 
 
-np.save('/Users/gordondoore/Documents/GitHub/waves-summer-2024/camera_matrix_5k.npy', mtx)
-np.save('/Users/gordondoore/Documents/GitHub/waves-summer-2024/dist_coeff_5k.npy', dist)
-np.save('/Users/gordondoore/Documents/GitHub/waves-summer-2024/rvecs_5k.npy', rvecs)
-np.save('/Users/gordondoore/Documents/GitHub/waves-summer-2024/tvecs_5k.npy', tvecs)
+if __name__ == '__main__':
+    calibrate_camera('calibration/acortiz@colbydotedu_CALIB/calib_frames_5k','calibration/acortiz@colbydotedu_CALIB/hmm','5k_', (7,9))
+
